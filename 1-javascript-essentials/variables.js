a625dfc6cfbd617f7e4b3d402b5324921e2625bd
Joao Victor Ignacio <joao.ignacio@venturus.org.br>
2020-03-16 17:38:41 -0300

    Added NestJs Project

--- 1-javascript-essentials/variables.js
+++ 1-javascript-essentials/variables.js
@@ -4,9 +4,15 @@ var variable1 = "Pablo";
 
 // >= ES6
 // Não pode ser 're-assignada'
-const variable2 = "Rafael";
+const variable2 = {
+  name: "João Victor",
+  age: 30
+};
+
 // Pode ser 're-assignada'
 let variable3 = 532.22;
+
+variable2.lastName = "Oakenshield";
 
 console.log(variable1);
 console.log(variable2);
